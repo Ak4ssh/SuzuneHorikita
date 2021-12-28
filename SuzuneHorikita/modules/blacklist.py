0cd0d1255b8d57920349703b7662f5f4ec853464
desinobita <84653553+desinobita@users.noreply.github.com>
2021-12-28 20:01:01 +0530

    Update blacklist.py

--- SuzuneHorikita/modules/blacklist.py
+++ SuzuneHorikita/modules/blacklist.py
@@ -487,7 +487,7 @@ Note:
 
 ✗ *<sticker link> can be* `https://t.me/addstickers/<sticker>` *or just* `<sticker>` *or reply to the sticker message*
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✗ By Suzune Team~*
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
     "blacklist",
