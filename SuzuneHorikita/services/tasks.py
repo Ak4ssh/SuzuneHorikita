469355336bc301e5d8821fffab4ad20fb6427256
desinobita <84653553+desinobita@users.noreply.github.com>
2021-12-28 16:53:01 +0530

    Update tasks.py

--- SuzuneHorikita/services/tasks.py
+++ SuzuneHorikita/services/tasks.py
@@ -2,8 +2,8 @@ from asyncio import Lock, create_task
 from time import time
 from pyrogram import filters
 from pyrogram.types import Message
-from Yuriko import BOT_ID, BOT_USERNAME, DEV_USERS
-from Yuriko.services.sections import bold, section, w
+from SuzuneHorikita import BOT_ID, BOT_USERNAME, DEV_USERS
+from SuzuneHorikita.services.sections import bold, section, w
 tasks = {}
 TASKS_LOCK = Lock()
 arrow = lambda x: (x.text if x else "") + "\n`→`"
