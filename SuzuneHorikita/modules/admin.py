4728c04e227cd70b2ed9e72468eef3ebb974f8fa
desinobita <84653553+desinobita@users.noreply.github.com>
2021-12-28 18:53:49 +0530

    Update admin.py

--- SuzuneHorikita/modules/admin.py
+++ SuzuneHorikita/modules/admin.py
@@ -207,14 +207,14 @@ def promote(update: Update, context: CallbackContext) -> str:
         bot.promoteChatMember(
             chat.id,
             user_id,
-            can_change_info=bot_member.can_change_info,
             can_post_messages=bot_member.can_post_messages,
             can_edit_messages=bot_member.can_edit_messages,
             can_delete_messages=bot_member.can_delete_messages,
             can_invite_users=bot_member.can_invite_users,
-            # can_promote_members=bot_member.can_promote_members,
-            can_restrict_members=bot_member.can_restrict_members,
+            # can_promote_members=bot_member.can_promote_member,
             can_pin_messages=bot_member.can_pin_messages,
+            can_manage_voice_chats=bot_member.can_manage_voice_chats
+            
         )
     except BadRequest as err:
         if err.message == "User_not_mutual_contact":
