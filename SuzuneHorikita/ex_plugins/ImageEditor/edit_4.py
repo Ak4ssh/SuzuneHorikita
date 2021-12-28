d74ba49830c01b559d791c87a6ef17925c64b9e7
desinobita <84653553+desinobita@users.noreply.github.com>
2021-12-28 16:20:51 +0530

    Update edit_4.py

--- SuzuneHorikita/ex_plugins/ImageEditor/edit_4.py
+++ SuzuneHorikita/ex_plugins/ImageEditor/edit_4.py
@@ -8,7 +8,7 @@ import numpy as np
 import requests
 from PIL import Image, ImageDraw, ImageOps
 
-from Yuriko.confing import get_str_key
+from SuzuneHorikita.confing import get_str_key
 
 RemoveBG_API = get_str_key("REM_BG_API_KEY", required=False)
 
