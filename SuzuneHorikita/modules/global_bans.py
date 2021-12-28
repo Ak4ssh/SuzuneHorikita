521b8353c17c16dea2f8b4bb7437f10e6aeb870b
desinobita <84653553+desinobita@users.noreply.github.com>
2021-12-28 20:40:01 +0530

    Update global_bans.py

--- SuzuneHorikita/modules/global_bans.py
+++ SuzuneHorikita/modules/global_bans.py
@@ -540,19 +540,8 @@ def __chat_settings__(chat_id, user_id):
 __help__ = f"""
 *Admins only:*
 
-✗ /antispam - `<on/off/yes/no> Will toggle our antispam tech or return your current settings.`
-
-`Anti-Spam used by bot devs to ban spammers across all groups. This helps protect`
-`you and your groups by removing spam flooders as quickly as possible.`
-*Note:* `Users can appeal gbans or report spammers at` @{SUPPORT_CHAT}
-
-`This also integrates` @Spamwatch `API to remove Spammers as much as possible from your chatroom!`
-*What is SpamWatch?*
-`SpamWatch maintains a large constantly updated ban-list of spambots trolls bitcoin spammers and unsavoury characters`
-`Constantly help banning spammers off from your group automatically So you wont have to worry about spammers storming your group.`
-*Note:* `Users can appeal spamwatch bans at` @DeeCodeBots
-
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+✗ /antispam - `<on/off/yes/no> Will toggle our antispam tech or return your current setting
+*✗ By Suzune Team~*
 """
 
 GBAN_HANDLER = CommandHandler("gban", gban, run_async=True)
