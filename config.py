880542ed30d0d17ddbf40a46bf070ada8e1a3620
desinobita <84653553+desinobita@users.noreply.github.com>
2022-01-11 16:18:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config(object):
 	API_ID = int(os.environ.get("API_ID", "1234"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	MONGO_DB_URI = os.environ.get("MONGO_DB_URI")
