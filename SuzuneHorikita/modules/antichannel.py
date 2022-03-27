75cc6e1123f0d5e15e3a91ea5128b4c8e57786bd
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-03-27 12:04:41 -0700

    hm done added anti channel mode with tgdb

--- SuzuneHorikita/modules/antichannel.py
+++ SuzuneHorikita/modules/antichannel.py
@@ -1,92 +1,78 @@
-import html
+"""
+Copyright (c) 2021 Itzz-axel
+This is part of @VexanaFAnClub so don't change anything....
+"""
 
-from telegram import Update
-from telegram.ext import CallbackContext
-from telegram.ext.filters import Filters
-from telegram.ext import CallbackContext, Filters, CommandHandler, run_async, CallbackQueryHandler
+import asyncio
+from pyrogram import filters
+from SuzuneHorikita import pbot as app
+from pyrogram.types import Message
+from SuzuneHorikita.utils.errors import capture_err
 
-from SuzuneHorikita.AntiChannel.anonymous import AdminPerms, user_admin
-from SuzuneHorikita.AntiChannel.decorators import shasacmd, shasamsg
-from SuzuneHorikita.modules.sql.antichannel_sql import (
-    antichannel_status,
-    disable_antichannel,
-    enable_antichannel,
-)
+active_channel = []
 
-from SuzuneHorikita import (
-    DEV_USERS,
-    LOGGER,
-    OWNER_ID,
-    DRAGONS,
-    DEMONS,
-    TIGERS,
-    WOLVES,
-    dispatcher,
-)
 
-from SuzuneHorikita.modules.log_channel import gloggable, loggable
-from SuzuneHorikita.modules.helper_funcs.chat_status import (
-    user_admin_no_reply,
-    bot_admin,
-    can_restrict,
-    connection_status,
-    is_user_admin,
-    is_user_ban_protected,
-    is_user_in_chat,
-    user_admin,
-    user_can_ban,
-    can_delete,
-    dev_plus,
-)
+async def eor(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
 
-@connection_status
-@bot_admin
-@can_restrict
-@user_admin
-@user_can_ban
-@loggable
-@dev_plus
-def antichannel(update: Update, context: CallbackContext):
-    message = update.effective_message
-    chat = update.effective_chat
-    args = context.args
-    if len(args) > 0:
-        s = args[0].lower()
-        if s in ["yes", "on"]:
-            enable_antichannel(chat.id)
-            message.reply_html(
-                "Enabled antichannel in {}".format(html.escape(chat.title))
-            )
-        elif s in ["off", "no"]:
-            disable_antichannel(chat.id)
-            message.reply_html(
-                "Disabled antichannel in {}".format(html.escape(chat.title))
-            )
-        else:
-            message.reply_text("Unrecognized arguments {}".format(s))
+
+
+async def channel_toggle(db, message: Message):
+    status = message.text.split(None, 1)[1].lower()
+    chat_id = message.chat.id
+    if status == "on":
+        if chat_id not in db:
+            db.append(chat_id)
+            text = "**ᴀɴᴛɪ ᴄʜᴀɴɴᴇʟ ᴍᴏᴅᴇ ᴇɴᴀʙʟᴇ ✅. I will delete all message that send with channel names in {message.chat.id} .**"
+            return await eor(message, text=text)
+        await eor(message, text="ᴀɴᴛɪ ᴄʜᴀɴɴᴇʟ ᴍᴏᴅᴇ ᴇɴᴀʙʟᴇ .")
+    elif status == "off":
+        if chat_id in db:
+            db.remove(chat_id)
+            return await eor(message, text="ᴀɴᴛɪ ᴄʜᴀɴɴᴇʟ ᴍᴏᴅᴇ ᴅɪsᴀʙʟᴇ!")
+        await eor(message, text=f"**ᴀɴᴛɪ ᴄʜᴀɴɴᴇʟ ᴍᴏᴅᴇ ᴅɪsᴀʙʟᴇᴅ ɪɴ ᴛʜᴇ ᴄʜᴀᴛt** {message.chat.id} ❌")
+    else:
+        await eor(message, text="ᴘʟᴇᴀsᴇ ᴜsᴇ `/antichannel on` ᴏʀ  off` ᴏɴʟʏ ")
+
+
+# Enabled | Disable antichannel
+
+
+@app.on_message(filters.command("antichannel") & ~filters.edited)
+@capture_err
+async def antichannel_status(_, message: Message):
+    if len(message.command) != 2:
+        return await eor(message, text="ᴘʟᴇᴀsᴇ ᴜsᴇ `/antichannel on` ᴏʀ  off` ᴏɴʟʏ")
+    await channel_toggle(active_channel, message)
+
+
+
+
+@app.on_message(filters.text & ~filters.linked_channel, group=36)        
+@app.on_message(filters.media & ~filters.linked_channel, group=36)
+@app.on_message(filters.sticker & ~filters.linked_channel, group=36)
+@app.on_message(filters.via_bot & ~filters.linked_channel, group=36)
+async def anitchnl(_, message):
+  chat_id = message.chat.id
+  if message.sender_chat:
+    sender = message.sender_chat.id 
+    if message.chat.id not in active_channel:
         return
-    message.reply_html(
-        "Antichannel setting is currently {} in {}".format(
-            antichannel_status(chat.id), html.escape(chat.title)
-        )
-    )
-   
-def eliminate_channel(update: Update, context: CallbackContext):
-    message = update.effective_message
-    chat = update.effective_chat
-    bot = context.bot
-    if not antichannel_status(chat.id):
+    if chat_id == sender:
         return
-    if (
-        message.sender_chat
-        and message.sender_chat.type == "channel"
-        and not message.is_automatic_forward
-    ):
-        message.delete()
-        sender_chat = message.sender_chat
-        bot.ban_chat_sender_chat(sender_chat_id=sender_chat.id, chat_id=chat.id)
+    else:
+        await message.delete()
+        ti = await message.reply_text("*ᴄʜᴀɴɴᴇʟ ᴍᴇss ʜᴀs ʙᴇᴇɴ ᴅᴇᴛᴇᴛᴇᴄᴛᴇᴅ ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇsғᴜʟʟʏ . **")
+        await asyncio.sleep(7)
+        await ti.delete()        
+        
+__help__ = """
+*User Commands:*
+❂ /antichannel on *:*Toggle anti channel mode 
+❂ /antichannel on *:*Disable anti channel mode 
 
+"""
 
-ANTICHANNEL_HANDLER = CommandHandler(["antichannel"], antichannel, run_async=True)
-
-dispatcher.add_handler(ANTICHANNEL_HANDLER)
+__mod_name__ = "anti channel"        
