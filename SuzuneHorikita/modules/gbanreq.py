d8ca82de552f18ae2c34dc6d49f79b3304fb26e2
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-28 10:46:00 +0530

    Add files via upload

--- SuzuneHorikita/modules/gbanreq.py
+++ SuzuneHorikita/modules/gbanreq.py
@@ -1,76 +1,186 @@
-import html
-import json
 import os
-from typing import Optional
+from database import db
+from pyrogram import Client, filters
+from config import Config
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import logging
+from logging.handlers import RotatingFileHandler
+import html
+import time
+from datetime import datetime
+from io import BytesIO
 
+from telegram import ParseMode, Update
+from telegram.error import BadRequest, TelegramError, Unauthorized
+from telegram.ext import (
+    CallbackContext,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.utils.helpers import mention_html
+
+import SuzuneHorikita.modules.sql.global_bans_sql as sql
+from SuzuneHorikita.modules.sql.users_sql import get_user_com_chats
 from SuzuneHorikita import (
     DEV_USERS,
+    EVENT_LOGS,
     OWNER_ID,
+    STRICT_GBAN,
     DRAGONS,
     SUPPORT_CHAT,
+    SPAMWATCH_SUPPORT_CHAT,
     DEMONS,
     TIGERS,
     WOLVES,
+    sw,
     dispatcher,
 )
 from SuzuneHorikita.modules.helper_funcs.chat_status import (
-    dev_plus,
-    sudo_plus,
-    whitelist_plus,
+    is_user_admin,
     support_plus,
+    user_admin,
 )
-from SuzuneHorikita.modules.helper_funcs.extraction import extract_user
-from SuzuneHorikita.modules.log_channel import gloggable
-from telegram import ParseMode, TelegramError, Update
-from telegram.ext import CallbackContext, CommandHandler
-from telegram.utils.helpers import mention_html
+from SuzuneHorikita.modules.helper_funcs.extraction import (
+    extract_user,
+    extract_user_and_text,
+)
+from SuzuneHorikita.modules.helper_funcs.misc import send_to_list
 
-ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "SuzuneHorikita/elevated_users.json")
+GBAN_ENFORCE_GROUP = 6
 
-
-def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
-    bot = context.bot
-    if not user_id:
-        reply = "That...is a chat! baka ka omae?"
-
-    elif user_id == bot.id:
-        reply = "This does not work that way."
-
-    else:
-        reply = None
-    return reply
+GBAN_ERRORS = {
+    "User is an administrator of the chat",
+    "Chat not found",
+    "Not enough rights to restrict/unrestrict chat member",
+    "User_not_participant",
+    "Peer_id_invalid",
+    "Group chat was deactivated",
+    "Need to be inviter of a user to kick it from a basic group",
+    "Chat_admin_required",
+    "Only the creator of a basic group can kick group administrators",
+    "Channel_private",
+    "Not in the chat",
+    "Can't remove chat owner",
+}
 
 @support_plus
-@gloggable
-def gbanreq(update: Update, context: CallbackContext) -> str:
+def gbanreq(update: Update, context: CallbackContext):
+    bot, args = context.bot, context.args
     message = update.effective_message
     user = update.effective_user
     chat = update.effective_chat
-    bot, args = context.bot, context.args
-    user_id = extract_user(message, args)
-    user_member = bot.getChat(user_id)
-    rt = ""
+    log_message = ""
 
-    update.effective_message.reply_text(
-        rt
-        + "\nSuccessfully Sent Your Request".format(
-            user_member.first_name,
-        ),
-    )
+    user_id, reason = extract_user_and_text(message, args)
 
-    log_message = (
-        f"▪︎ GBAN REQUEST\n"
-        f"<b>▪︎Requested By:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
-        f"<b>▪︎Victim:</b> {mention_html(user_member.id, html.escape(user_member.first_name))}"
-    )
+    if not user_id:
+        message.reply_text(
+            "You don't seem to be referring to a user or the ID specified is incorrect..",
+        )
+        return
+
+    if int(user_id) in DEV_USERS:
+        message.reply_text(
+            "That user is part of the Association\nI can't act against our own.",
+        )
+        return
+
+    if int(user_id) in DRAGONS:
+        message.reply_text(
+            "I spy, with my little eye... a disaster! Why are you guys turning on each other?",
+        )
+        return
+
+    if int(user_id) in DEMONS:
+        message.reply_text(
+            "OOOH someone's trying to gban a Demon Disaster! *grabs popcorn*",
+        )
+        return
+
+    if int(user_id) in TIGERS:
+        message.reply_text("That's a Tiger! They cannot be banned!")
+        return
+
+    if int(user_id) in WOLVES:
+        message.reply_text("That's a Wolf! They cannot be banned!")
+        return
+
+    if user_id == bot.id:
+        message.reply_text("You uhh...want me to punch myself?")
+        return
+
+    if user_id in [1789859817]:
+        message.reply_text("Sike! That's The Wrong Number.")
+        return
+
+    try:
+        user_chat = bot.get_chat(user_id)
+    except BadRequest as excp:
+        if excp.message == "User not found":
+            message.reply_text("I can't seem to find this user.")
+            return ""
+        return
+
+    if user_chat.type != "private":
+        message.reply_text("That's not a user!")
+        return
+
+    if sql.is_user_gbanned(user_id):
+
+        if not reason:
+            message.reply_text(
+                "This user is already gbanned; I'd change the reason, but you haven't given me one...",
+            )
+            return
+
+        old_reason = sql.update_gban_reason(
+            user_id,
+            user_chat.username or user_chat.first_name,
+            reason,
+        )
+        if old_reason:
+            message.reply_text(
+                "This user Complaint Has Already Been Send, for the following reason:\n"
+                "<code>{}</code>\n"
+                "I've gone and updated it with your new reason!".format(
+                    html.escape(old_reason),
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+        else:
+            message.reply_text(
+                "This user is already gbanned, but had no reason set; I've gone and updated it!",
+            )
+
+        return
+
+    message.reply_text("On it!")
+
+    start_time = time.time()
+    datetime_fmt = "%Y-%m-%dT%H:%M"
+    current_time = datetime.utcnow().strftime(datetime_fmt)
 
     if chat.type != "private":
-        log_message = f"<b>{html.escape(chat.title)}:</b>\n" + log_message
+        chat_origin = "<b>{} ({})</b>\n".format(html.escape(chat.title), chat.id)
+    else:
+        chat_origin = "<b>{}</b>\n".format(chat.id)
 
-    return log_message
+    log_message = (
+        f"Gban Request\n"
+        f"<b>Originated from:</b> <code>{chat_origin}</code>\n"
+        f"<b>Requested By:</b> {mention_html(user.id, user.first_name)}\n"
+        f"<b>Victim:</b> {mention_html(user_chat.id, user_chat.first_name)}\n"
+        f"<b>Victim User ID:</b> <code>{user_chat.id}</code>\n"
+        f"<b>Event Stamp:</b> <code>{current_time}</code>"
+    )
 
-
+    if reason:
+        if chat.type == chat.SUPERGROUP and chat.username:
+            log_message += f'\n<b>Reason:</b> <a href="https://telegram.me/{chat.username}/{message.message_id}">{reason}</a>'
+        else:
+            log_message += f"\n<b>Reason:</b> <code>{reason}</code>"
 
 GBANREQ_HANDLER = CommandHandler(("gban", "req"), gbanreq, run_async=True)
 dispatcher.add_handler(GBANREQ_HANDLER)
-
