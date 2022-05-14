7d1ba232fbf9d7ac5c5b82fceadab4ea25a238eb
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-15 00:13:41 +0530

    Update paste.py

--- SuzuneHorikita/modules/paste.py
+++ SuzuneHorikita/modules/paste.py
@@ -1,6 +1,6 @@
 import requests
-from SuzuneBot import dispatcher
-from SuzuneBot.modules.disable import DisableAbleCommandHandler
+from SuzuneHorikita import dispatcher
+from SuzuneHorikita.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
