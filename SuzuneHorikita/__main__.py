5f62c2b6510ae7cb4cccc74c374b17c8d00590a8
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-05-15 11:53:04 -0700

    Small update
    two praser passing resolved and added img auto vars

--- SuzuneHorikita/__main__.py
+++ SuzuneHorikita/__main__.py
@@ -235,12 +235,9 @@ def start(update: Update, context: CallbackContext):
 
         else:
             first_name = update.effective_user.first_name
-            update.effective_message.reply_text(
-                PM_START_TEXT.format(
-                    escape_markdown(first_name),
-                    escape_markdown(uptime),
-                    sql.num_users(),
-                    sql.num_chats()),                        
+            update.effective_message.reply_photo(
+                SuzuneHorikitarobot_IMG,
+                PM_START_TEXT,
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
