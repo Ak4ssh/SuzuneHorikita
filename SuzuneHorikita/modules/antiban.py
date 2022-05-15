af39008bcce05d2d70230e85a607a9a15fc8dad3
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-05-15 12:05:21 -0700

    added anti channel support in regular db.
    db.append add chat_id

--- SuzuneHorikita/modules/antiban.py
+++ SuzuneHorikita/modules/antiban.py
@@ -1,58 +1,66 @@
-from pyrogram import Client, filters
-from pyrogram.raw.base import Update
-from pyrogram.raw import types, functions
+"""
+Copyright (c) 2022 Axel (aksr-aashish)
+This is part of @vexanafanclub so don't change anything....
+"""
+
+import asyncio
+from pyrogram import filters
+from vexana import pbot as app
+from pyrogram.types import Message
+from SuzuneHorikita.utils.errors import capture_err
+
+active_channel = []
+
+
+async def eor(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
+
+
+async def channel_toggle(db, message: Message):
+    status = message.text.split(None, 1)[1].lower()
+    chat_id = message.chat.id
+    if status == "on":
+        if chat_id not in db:
+            db.append(chat_id)
+            text = "**ChannelMode` enabled`.**"
+            return await eor(message, text=text)
+        await eor(message, text="`ChannelMode` Enabled.")
+    elif status == "off":
+        if chat_id in db:
+            db.remove(chat_id)
+            return await eor(message, text=" ChannelMode Disabled!")
+        await eor(message, text=f"**ChannelMode Disabled in :- ** {message.chat.id} ❌")
+    else:
+        await eor(message, text="Error Cant Use this way\n\n1->`/channel on` \n 2->`/channel off`\n\nNote :- Cmds are not case `sensitive`")
 
 
 
-@Client.on_raw_update()
-async def channel_handler(client: Client, update: Update, _, chats: dict):
-    while True:
-        try:
-            # Check for message that are from channel
-            if not isinstance(update, types.UpdateNewChannelMessage) or not isinstance(
-                update.message.from_id, types.PeerChannel
-            ):
-                return
-            # Basic data
-            message = update.message
-            chat_id = int(f"-100{message.peer_id.channel_id}")
-            channel_id = int(f"-100{message.from_id.channel_id}")
-            # Check enable or not
-            # Check for linked or free channel
-            if (
-                (
-                    message.fwd_from
-                    and message.fwd_from.saved_from_peer
-                    == message.fwd_from.from_id
-                    == message.from_id
-                )
-                or channel_id == chat_id
-            ):
-                return
-            # Delete the message sent by channel and ban it
-            await client.send(
-                functions.channels.EditBanned(
-                    channel=await client.resolve_peer(chat_id),
-                    participant=await client.resolve_peer(channel_id),
-                    banned_rights=types.ChatBannedRights(
-                        until_date=0,
-                        view_messages=True,
-                        send_messages=True,
-                        send_media=True,
-                        send_stickers=True,
-                        send_gifs=True,
-                        send_games=True,
-                        send_polls=True,
-                    ),
-                )
-            )
-            await client.delete_messages(chat_id, message.id)
-            await client.send_message(
-                int(chat_id),
-                f"#𝙰𝙽𝚃𝙸𝙲𝙷𝙰𝙽𝙽𝙴𝙻\n\n᛭ 𝚂𝙴𝙽𝙳𝙴𝚁 𝙸𝙳: `{channel_id}`\n᛭ 𝚃𝙰𝙺𝙴𝙽 𝙰𝙲𝚃𝙸𝙾𝙽: `DELETE BAN`",
-                disable_web_page_preview=True,
-            )
-            break
-        except Exception as e:
-            print(e)
-            break
+@app.on_message(filters.command("channel") & ~filters.edited)
+@capture_err
+async def channel_status(_, message: Message):
+    if len(message.command) != 2:
+        return await eor(message, text="Error Cant Use this way\n\n1->`/channel on` \n 2->`/channel off`\n\nNote :- Cmds are not case `sensitive`")
+    await channel_toggle(active_channel, message)
+
+
+
+
+@app.on_message(filters.text & ~filters.linked_channel, group=36)        
+@app.on_message(filters.media & ~filters.linked_channel, group=36)
+@app.on_message(filters.sticker & ~filters.linked_channel, group=36)
+@app.on_message(filters.via_bot & ~filters.linked_channel, group=36)
+async def anitchnl(_, message):
+  chat_id = message.chat.id
+  if message.sender_chat:
+    sender = message.sender_chat.id 
+    if message.chat.id not in active_channel:
+        return
+    if chat_id == sender:
+        return
+    else:
+        await message.delete()
+        ti = await message.reply_text("**Chaneel Mess Deleted! chat id = '{chat_id}**")
+        await asyncio.sleep(4)
+        await ti.delete()        
