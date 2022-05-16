0e1478ba39685aa042bdc2908a077436018239b3
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-16 11:23:09 +0530

    Update wish.py

--- SuzuneHorikita/modules/wish.py
+++ SuzuneHorikita/modules/wish.py
@@ -9,5 +9,5 @@ from SuzuneHorikita import telethn as tbot
 @register(pattern=("/wish"))
 async def awake(event):
     rem = randint(1, 100)
-    await tbot.reply(f"Your wish has been cast.✨\n\nchance of success: {rem}% ")
+    await tbot.reply_text(f"Your wish has been cast.✨\n\nchance of success: {rem}% ")
 
