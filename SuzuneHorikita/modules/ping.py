146ae30432adf24f1532ceeccd096b7e07a9ea27
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-16 11:45:53 +0530

    Update ping.py

--- SuzuneHorikita/modules/ping.py
+++ SuzuneHorikita/modules/ping.py
@@ -4,10 +4,12 @@ from typing import List
 import requests
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
-
+from SuzuneHorikita.events import register
 from SuzuneHorikita import StartTime, dispatcher
 from SuzuneHorikita.modules.helper_funcs.chat_status import sudo_plus
 from SuzuneHorikita.modules.disable import DisableAbleCommandHandler
+from SuzuneHorikita import telethn as tbot
+from random import randint 
 
 
 @sudo_plus
@@ -26,6 +28,24 @@ def ping(update: Update, context: CallbackContext):
         "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
         parse_mode=ParseMode.HTML,
     )
+
+@register(pattern=("/wish"))
+async def awake(event):
+    msg = update.effective_message
+    rem = randint(1, 100)
+    start_time = time.time()
+    message = msg.reply_text(f"Your wish has been cast.✨\n\nchance of success: {rem}% ")
+    end_time = time.time()
+    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
+    uptime = get_readable_time((time.time() - StartTime))
+
+    message.edit_text(
+        "<b>Pong!!</b>\n"
+        "<b>Time Taken:</b> <code>{}</code>\n"
+        "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
+        parse_mode=ParseMode.HTML,
+    )
+
 
 
 
