d92960641553c26164597a488bfba67cc11a38c5
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-16 09:54:06 +0530

    Update __main__.py

--- SuzuneHorikita/__main__.py
+++ SuzuneHorikita/__main__.py
@@ -241,7 +241,6 @@ def start(update: Update, context: CallbackContext):
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
                 timeout=60,
-                disable_web_page_preview=False,
             )
     else:
         update.effective_message.reply_photo(
