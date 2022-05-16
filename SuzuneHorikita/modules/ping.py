0c32389789c23c12222798cb7e785de4f378babb
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-05-16 12:32:27 +0530

    Update ping.py

--- SuzuneHorikita/modules/ping.py
+++ SuzuneHorikita/modules/ping.py
@@ -11,24 +11,6 @@ from SuzuneHorikita.modules.disable import DisableAbleCommandHandler
 from SuzuneHorikita import telethn as tbot
 from random import randint 
 
-
-@sudo_plus
-def ping(update: Update, context: CallbackContext):
-    msg = update.effective_message
-
-    start_time = time.time()
-    message = msg.reply_text("Pinging...")
-    end_time = time.time()
-    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
-    uptime = get_readable_time((time.time() - StartTime))
-
-    message.edit_text(
-        "<b>Pong!!</b>\n"
-        "<b>Time Taken:</b> <code>{}</code>\n"
-        "<b>Service Uptime:</b> <code>{}</code>".format(telegram_ping, uptime),
-        parse_mode=ParseMode.HTML,
-    )
-
 @register(pattern=("/wish"))
 def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
@@ -50,9 +32,9 @@ def ping(update: Update, context: CallbackContext):
 
 
 
-PING_HANDLER = DisableAbleCommandHandler("ping", ping, run_async=True)
+PING_HANDLER = DisableAbleCommandHandler("wish", ping, run_async=True)
 
 dispatcher.add_handler(PING_HANDLER)
 
-__command_list__ = ["ping"]
+__command_list__ = ["wish"]
 __handlers__ = [PING_HANDLER]
