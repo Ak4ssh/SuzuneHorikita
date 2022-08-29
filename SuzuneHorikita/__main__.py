de6bd4ac79ca0fb420a23be79a00a6b5bc2aa424
Captain Ezio <iamgojoof6eyes@gmail.com>
2022-08-29 21:40:57 +0530

    minor changes

--- SuzuneHorikita/__main__.py
+++ SuzuneHorikita/__main__.py
@@ -863,7 +863,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "*Back Online To Server ⚡*")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "**Back Online To Server**⚡")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
