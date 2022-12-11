89039fefef8a4be9e38a4b026f556e2a1a488319
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-12-11 20:26:13 +0530

    Update anonymous.py

--- src/source/helper_funcs/anonymous.py
+++ src/source/helper_funcs/anonymous.py
@@ -6,7 +6,7 @@ from telegram.ext import CallbackContext
 from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
 from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
 
-from src import DEV_USERS, DRAGONS, dispatcher
+from src import Dev, DRAGONS, dispatcher
 from src.source.helper_funcs.decorators import Kaguyacallback
 
 
