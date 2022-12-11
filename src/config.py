668a91ed95ddb4a7d1b265e5867fde547dd4d9e2
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-12-11 13:51:17 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,10 +1,9 @@
-# Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
 import json
 import os
 
 
 def get_user_list(config, key):
-    with open("{}/SuzuneHorikita/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/src/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
