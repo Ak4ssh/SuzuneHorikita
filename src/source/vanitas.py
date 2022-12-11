8877a25b908f18e0bcafa5b2cf54e68fca4b0554
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-12-11 14:49:46 +0530

    Update vanitas.py

--- src/source/vanitas.py
+++ src/source/vanitas.py
@@ -9,7 +9,7 @@ from telegram.ext.filters import Filters
 from telegram.parsemode import ParseMode
 
 import src.source.sql.vanitas_sql as sql
-from vanitas import User as Antispam
+from vanitas import User as antispam
 
 from time import sleep
 from telegram import ParseMode
@@ -22,36 +22,36 @@ from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.utils.helpers import mention_html, mention_markdown, escape_markdown
 
 from src.source.helper_funcs.filters import CustomFilters
-from src.source.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from src.source.helper_funcs.chat_status import TheRiZoeL, TheRiZoeL_no_reply
 from src import dispatcher, updater, SUPPORT_CHAT, SYL
 from src.source.log_channel import gloggable
 
-v = Antispam()
+v = antispam()
 
-logging.info("AntiSpam System On")
+logging.info("antispam System On")
 
 
-@user_admin_no_reply
+@TheRiZoeL_no_reply
 @gloggable
-def Antispamrm(update: Update, context: CallbackContext) -> str:
+def antispamrm(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
-    match = re.match(r"rm_Antispam\((.+?)\)", query.data)
+    match = re.match(r"rm_antispam\((.+?)\)", query.data)
     if match:
         user_id = match.group(1)
         chat: Optional[Chat] = update.effective_chat
 
-        is_Antispam = sql.rem_Antispam(chat.id)
-        if is_Antispam:
-            is_Antispam = sql.rem_Antispam(chat.id)
+        is_antispam = sql.rem_antispam(chat.id)
+        if is_antispam:
+            is_antispam = sql.rem_antispam(chat.id)
             return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
-                f"Antispam_BAN_DISABLED\n"
+                f"antispam_BAN_DISABLED\n"
                 f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
         else:
             update.effective_message.reply_text(
-                "Antispam System Disable by {}".format(
+                "antispam System Disable by {}".format(
                     mention_html(user.id, user.first_name)
                 ),
                 parse_mode=ParseMode.HTML,
@@ -61,27 +61,27 @@ def Antispamrm(update: Update, context: CallbackContext) -> str:
     return ""
 
 
-@user_admin_no_reply
+@TheRiZoeL_no_reply
 @gloggable
-def Antispamadd(update: Update, context: CallbackContext) -> str:
+def antispamadd(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
-    match = re.match(r"add_Antispam\((.+?)\)", query.data)
+    match = re.match(r"add_antispam\((.+?)\)", query.data)
     if match:
         user_id = match.group(1)
         chat: Optional[Chat] = update.effective_chat
 
-        is_Antispam = sql.set_Antispam(chat.id)
-        if is_Antispam:
-            is_Antispam = sql.set_Antispam(chat.id)
+        is_antispam = sql.set_antispam(chat.id)
+        if is_antispam:
+            is_antispam = sql.set_antispam(chat.id)
             return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
-                f"Antispam_BAN_ENABLE\n"
+                f"antispam_BAN_ENABLE\n"
                 f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
         else:
             update.effective_message.reply_text(
-                "Antispam System Enable by {}".format(
+                "antispam System Enable by {}".format(
                     mention_html(user.id, user.first_name)
                 ),
                 parse_mode=ParseMode.HTML,
@@ -98,11 +98,11 @@ def bluemoon(update: Update, context: CallbackContext):
     keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(text="Modes", callback_data="enabledisablebutton_Antispam({})"),
-                InlineKeyboardButton(text="About", callback_data="about_Antispam({})")]])
+                InlineKeyboardButton(text="Modes", callback_data="enabledisablebutton_antispam({})"),
+                InlineKeyboardButton(text="About", callback_data="about_antispam({})")]])
     message.reply_video(
         video,
-        caption="Welcome To The Antispam Module\n Antispam Is Antispam System",
+        caption="Welcome To The antispam Module\n antispam Is antispam System",
         reply_markup=keyboard,
         parse_mode=ParseMode.HTML,
     )
@@ -111,21 +111,21 @@ def bluemoon(update: Update, context: CallbackContext):
 def vantiasban(update: Update, context: CallbackContext):
     user = update.effective_user
     chat_id = update.effective_chat.id
-    is_Antispam = sql.is_Antispam(chat_id)
-    if not is_Antispam:
-        set_Antispam = sql.set_Antispam(chat_id)
-        if set_Antispam:       
+    is_antispam = sql.is_antispam(chat_id)
+    if not is_antispam:
+        set_antispam = sql.set_antispam(chat_id)
+        if set_antispam:       
             update.effective_message.reply_text(
-                 "Antispam System Enable by {}".format(
+                 "antispam System Enable by {}".format(
                     mention_html(user.id, user.first_name)
                 ),
                 parse_mode=ParseMode.HTML,
             )
     else:
-        rm_Antispam = sql.rm_Antispam(chat_id)
-        if rm_Antispam:
+        rm_antispam = sql.rm_antispam(chat_id)
+        if rm_antispam:
             update.effective_message.reply_text(
-                 "Antispam System Disable by {}".format(
+                 "antispam System Disable by {}".format(
                     mention_html(user.id, user.first_name)
                 ),
                 parse_mode=ParseMode.HTML,
@@ -134,20 +134,20 @@ def vantiasban(update: Update, context: CallbackContext):
     return ""
 
 
-@user_admin_no_reply
+@TheRiZoeL_no_reply
 def enabledisable(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
     chat: Optional[Chat] = update.effective_chat
-    match = re.match(r"enabledisablebutton_Antispam\((.+?)\)", query.data)
+    match = re.match(r"enabledisablebutton_antispam\((.+?)\)", query.data)
     if match:
-        if not sql.is_Antispam(chat.id):
+        if not sql.is_antispam(chat.id):
             keyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(text="Disable", callback_data="rm_Antispam({})")]])
+                        InlineKeyboardButton(text="Disable", callback_data="rm_antispam({})")]])
             update.effective_message.reply_text(
-                "Connection to Antispam System can be turned Disable",
+                "Connection to antispam System can be turned Disable",
                 reply_markup=keyboard,
                 parse_mode=ParseMode.HTML,
             )
@@ -156,9 +156,9 @@ def enabledisable(update: Update, context: CallbackContext) -> str:
             keyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton(text="Enable", callback_data="add_Antispam({})")]])
+                        InlineKeyboardButton(text="Enable", callback_data="add_antispam({})")]])
             update.effective_message.reply_text(
-                "Disconnect to Antispam System can be turned Enable",
+                "Disconnect to antispam System can be turned Enable",
                 reply_markup=keyboard,
                 parse_mode=ParseMode.HTML,
             )
@@ -170,22 +170,22 @@ def enabledisable(update: Update, context: CallbackContext) -> str:
 def about(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
-    match = re.match(r"about_Antispam\((.+?)\)", query.data)
+    match = re.match(r"about_antispam\((.+?)\)", query.data)
     if match:
         keyboard = InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
-                    text="Support", url="https://t.me/Antispam_support"
+                    text="Support", url="https://t.me/antispam_support"
                 ),
-                InlineKeyboardButton(text="Report", url="https://t.me/Antispamreport"
+                InlineKeyboardButton(text="Report", url="https://t.me/antispamreport"
                 ),
-                InlineKeyboardButton(text="Log", url="https://t.me/Antispamlogs")
+                InlineKeyboardButton(text="Log", url="https://t.me/antispamlogs")
             ]
         ]
     )
         update.effective_message.reply_text(
-            "Antispam Is A Telegram Antispam System",
+            "antispam Is A Telegram antispam System",
             reply_markup=keyboard,
             parse_mode=ParseMode.HTML,
         )
@@ -200,8 +200,8 @@ def bluemoon_callback(update: Update, context: CallbackContext, should_message=T
     chat = update.effective_chat
     user = update.effective_user
 
-    is_Antispam = sql.is_Antispam(chat_id)
-    if is_Antispam:
+    is_antispam = sql.is_antispam(chat_id)
+    if is_antispam:
         return
         x = None
     try:
@@ -218,9 +218,9 @@ def bluemoon_callback(update: Update, context: CallbackContext, should_message=T
                 [
                     [
                         InlineKeyboardButton(
-                            text="Appeal Chat", url="https://t.me/Antispam_support"
+                            text="Appeal Chat", url="https://t.me/antispam_support"
                         ),
-                        InlineKeyboardButton(text="Modes", callback_data="enabledisablebutton_Antispam({})"
+                        InlineKeyboardButton(text="Modes", callback_data="enabledisablebutton_antispam({})"
                         )
                     ]
                 ]
@@ -228,7 +228,7 @@ def bluemoon_callback(update: Update, context: CallbackContext, should_message=T
             update.effective_message.reply_video(
                 alertvideo,
                 caption=f"<b>Alert</b>: This User Is Blacklisted\n"
-                f"<b>Mode</b>: Enable Modes Using /Antispam Disable mode or Change mode\n"
+                f"<b>Mode</b>: Enable Modes Using /antispam Disable mode or Change mode\n"
                 f"<b>User ID</b>: <code>{x.user}</code>\n"
                 f"<b>Enforcer</b>: <code>{x.enforcer}</code>\n"
                 f"<b>Reason</b>: <code>{x.reason}</code>\n"
@@ -239,16 +239,16 @@ def bluemoon_callback(update: Update, context: CallbackContext, should_message=T
         return
 
 
-AntispamBAN_HANDLER = CommandHandler("Antispamban", Antispamban, run_async=True)
-BLUEMOON_HANDLER = CommandHandler("Antispam", bluemoon, run_async=True)
-ADD_Antispam_HANDLER = CallbackQueryHandler(Antispamadd, pattern=r"add_Antispam", run_async=True)
-ENABLE_HANDLER = CallbackQueryHandler(enabledisable, pattern=r"enabledisablebutton_Antispam", run_async=True)
-ABOUT_HANDLER = CallbackQueryHandler(about, pattern=r"about_Antispam", run_async=True)
-RM_VAINITAS_HANDLER = CallbackQueryHandler(Antispamrm, pattern=r"rm_Antispam", run_async=True)
+antispamBAN_HANDLER = CommandHandler("antispamban", antispamban, run_async=True)
+BLUEMOON_HANDLER = CommandHandler("antispam", bluemoon, run_async=True)
+ADD_antispam_HANDLER = CallbackQueryHandler(antispamadd, pattern=r"add_antispam", run_async=True)
+ENABLE_HANDLER = CallbackQueryHandler(enabledisable, pattern=r"enabledisablebutton_antispam", run_async=True)
+ABOUT_HANDLER = CallbackQueryHandler(about, pattern=r"about_antispam", run_async=True)
+RM_VAINITAS_HANDLER = CallbackQueryHandler(antispamrm, pattern=r"rm_antispam", run_async=True)
 BLUEMOON_HANDLERK = MessageHandler(filters=Filters.all & Filters.group, callback=bluemoon_callback)
 
-dispatcher.add_handler(ADD_Antispam_HANDLER)
-dispatcher.add_handler(AntispamBAN_HANDLER)
+dispatcher.add_handler(ADD_antispam_HANDLER)
+dispatcher.add_handler(antispamBAN_HANDLER)
 dispatcher.add_handler(ENABLE_HANDLER)
 dispatcher.add_handler(ABOUT_HANDLER)
 dispatcher.add_handler(BLUEMOON_HANDLER)
