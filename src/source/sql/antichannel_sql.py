966fabc9637437dcdfe40be1796ec825e60925e7
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-12-11 20:23:58 +0530

    Update antichannel_sql.py

--- src/source/sql/antichannel_sql.py
+++ src/source/sql/antichannel_sql.py
@@ -4,7 +4,7 @@ from sqlalchemy import Boolean
 from sqlalchemy.sql.sqltypes import String
 from sqlalchemy import Column
 
-from src.modules.sql import BASE, SESSION
+from src.source.sql import BASE, SESSION
 
 
 class AntiChannelSettings(BASE):
