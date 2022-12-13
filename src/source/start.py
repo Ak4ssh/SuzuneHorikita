36bf874ce2396b6b60f0b3680c6f832ec48eb283
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-12-13 21:32:21 +0530

    Update start.py

--- src/source/start.py
+++ src/source/start.py
@@ -8,11 +8,11 @@ from pyrogram import __version__ as pyrover
 from src.events import register
 from src import telethn as tbot
 
-VENOM = "https://telegra.ph/file/7c97605753018dfa4f832.mp4"
+VENOM = "https://telegra.ph/file/e3cd9302d6c371593c50d.mp4"
 
 @register(pattern=("/start"))
 async def awake(event):
-  TEXT = f"Heya [{event.sender.first_name}](tg://user?id={event.sender.id}), My name is Suzune Horikita - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.\n\n"
-  TEXT += "Join my [News Channel](t.me/SuzuneSuperbot) to get information on all the latest updates."
-  BUTTON = [[Button.url("➕Add Me To Your Group➕", "https://t.me/Suzune_Superbot?startgroup=true"),]]
+  TEXT = f"Heya [{event.sender.first_name}](tg://user?id={event.sender.id}), My name is Miku - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.\n\n"
+  TEXT += "Join my [News Channel](t.me/astorbots) to get information on all the latest updates."
+  BUTTON = [[Button.url("➕Add Me To Your Group➕", "https://t.me/mikuprorobot?startgroup=true"),]]
   await tbot.send_file(event.chat_id, VENOM, caption=TEXT, buttons=BUTTON)
