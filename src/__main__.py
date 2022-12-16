f7a7f0559e6d39ad1c177ec7855572d6bada4564
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2022-12-16 14:27:00 +0530

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -145,7 +145,6 @@ def send_help(chat_id, text, keyboard=None):
     parse_mode=ParseMode.MARKDOWN,
     disable_web_page_preview=True,
     reply_markup=keyboard,
-    )
 
 
 def test(update: Update, context: CallbackContext):
