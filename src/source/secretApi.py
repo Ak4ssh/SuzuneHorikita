44a84b2ee9edb57aa9c2a8fe369686c3caaa2f17
Ezio <100378440+Ezio-commits@users.noreply.github.com>
2022-12-27 12:04:47 +0530

    Create secretApi.py

---
+++ src/source/secretApi.py
@@ -0,0 +1,23 @@
+import requests
+import urllib
+import asyncio
+import os
+from pyrogram import filters
+from src import TEMP_DOWNLOAD_DIRECTORY, app
+
+
+@app.on_message(filters.command("tumharimummykechuche"))
+async def boobs(client, message):
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
+    pic_loc = os.path.join(TEMP_DOWNLOAD_DIRECTORY, "bobs.jpg")
+    a = await message.reply_text("**chuche loading**")
+    await asyncio.sleep(0.5)
+    await a.edit("`chuche loading...`")
+    nsfw = requests.get("http://api.oboobs.ru/noise/1").json()[0]["preview"]
+    urllib.request.urlretrieve("http://media.oboobs.ru/{}".format(nsfw), pic_loc)
+    await client.send_photo(
+        message.chat.id, pic_loc, caption="**😳😋🤤**"
+    )
+    os.remove(pic_loc)
+    await a.delete()
