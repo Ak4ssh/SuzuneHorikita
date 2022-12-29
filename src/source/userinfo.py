d50dc994b6b4fcb26f63f72c2a342758e68d1475
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-12-29 09:45:48 +0530

    ginfo committed to pyrogram and shifted to different file

--- src/source/userinfo.py
+++ src/source/userinfo.py
@@ -174,44 +174,7 @@ def get_id(update: Update, context: CallbackContext):
         )
 
 
-@telethn.on(
-    events.NewMessage(
-        pattern="/ginfo ", from_users=(TIGERS or []) + (DRAGONS or []) + (DEMONS or []),
-    ),
-)
-async def group_info(event) -> None:
-    chat = event.text.split(" ", 1)[1]
-    try:
-        entity = await event.client.get_entity(chat)
-        totallist = await event.client.get_participants(
-            entity, filter=ChannelParticipantsAdmins,
-        )
-        ch_full = await event.client(GetFullChannelRequest(channel=entity))
-    except:
-        await event.reply(
-            "Can't for some reason, maybe it is a private one or that I am banned there.",
-        )
-        return
-    msg = f"**ID**: `{entity.id}`"
-    msg += f"\n**Title**: `{entity.title}`"
-    msg += f"\n**Datacenter**: `{entity.photo.dc_id}`"
-    msg += f"\n**Video PFP**: `{entity.photo.has_video}`"
-    msg += f"\n**Supergroup**: `{entity.megagroup}`"
-    msg += f"\n**Restricted**: `{entity.restricted}`"
-    msg += f"\n**Scam**: `{entity.scam}`"
-    msg += f"\n**Slowmode**: `{entity.slowmode_enabled}`"
-    if entity.username:
-        msg += f"\n**Username**: {entity.username}"
-    msg += "\n\n**Member Stats:**"
-    msg += f"\n`Admins:` `{len(totallist)}`"
-    msg += f"\n`Users`: `{totallist.total}`"
-    msg += "\n\n**Admins List:**"
-    for x in totallist:
-        msg += f"\n• [{x.id}](tg://user?id={x.id})"
-    msg += f"\n\n**Description**:\n`{ch_full.full_chat.about}`"
-    await event.reply(msg)
-
-
+#removed ginfo from userinfo shifting it to ginfo.py - @Yuu-456
 
 def gifid(update: Update, context: CallbackContext):
     msg = update.effective_message
