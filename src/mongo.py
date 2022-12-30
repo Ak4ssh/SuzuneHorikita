a2953ed845667de79b31798d46674c524cae3f92
Abhinav <113854226+DARK-KING-2304@users.noreply.github.com>
2022-12-30 23:25:12 +0530

    Update mongo.py

--- src/mongo.py
+++ src/mongo.py
@@ -6,6 +6,7 @@ from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 
+from src import db 
 from src import MONGO_DB_URI
 from src.confing import get_int_key, get_str_key
 
@@ -24,3 +25,26 @@ try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
     sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+
+coupledb = db.couple
+
+async def _get_lovers(chat_id: int):
+    lovers = await coupledb.find_one({"chat_id": chat_id})
+    if not lovers:
+        return {}
+    return lovers["couple"]
+
+
+async def get_couple(chat_id: int, date: str):
+    lovers = await _get_lovers(chat_id)
+    if date in lovers:
+        return lovers[date]
+    return False
+
+
+async def save_couple(chat_id: int, date: str, couple: dict):
+    lovers = await _get_lovers(chat_id)
+    lovers[date] = couple
+    await coupledb.update_one(
+        {"chat_id": chat_id}, {"$set": {"couple": lovers}}, upsert=True
+       ) 
