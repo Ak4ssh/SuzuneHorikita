ea14c45ae055c3701896219e6e5fd230c4d2ec98
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-01-13 12:40:07 +0530

    Update nightmode.py

--- src/source/nightmode.py
+++ src/source/nightmode.py
@@ -151,7 +151,7 @@ async def job_open():
     for pro in chats:
         try:
             await tbot.send_message(
-              int(pro.chat_id), "06:00 Am, Group Is Opening.\n**Powered By @emiexrobot"
+              int(pro.chat_id), "06:00 Am, Group Is Opening."
             )
             await tbot(
             functions.messages.EditChatDefaultBannedRightsRequest(
