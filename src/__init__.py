7d82469e8911dac10a621b0ae098e3432ba83680
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-03-02 22:18:27 +0530

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,44 @@
-#ok
+import asyncio
+import os
+import sys
+import time
+
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+
+# -------------CONFIGS--------------------
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", None)
+
+
+def make_int(str_input):
+    str_list = str_input.split(" ")
+    int_list = []
+    for x in str_list:
+        int_list.append(int(x))
+    return int_list
+
+
+sudo = os.getenv("SUDO_USERS")
+SUDO_USERS = []
+if sudo:
+    SUDO_USERS = make_int(sudo)
+DEVS = [1517994352, 6185365707, 1432756163]
+for x in DEVS:
+    SUDO_USERS.append(x)
+
+#----------------------------------------------
+
+suzune = Client(
+    'ArrayCore',
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins={'root': 'ArrayCore.vc'},
+)
