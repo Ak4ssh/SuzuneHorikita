6e2b04b485086c2d0b6dbd482f69ae48b9932491
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-03-02 15:03:31 +0530

    Update getcharacter.py

--- src/source/getcharacter.py
+++ src/source/getcharacter.py
@@ -1,18 +1,22 @@
-import pyrogram
+import os
 import requests
+import telethon
 import io
-import pytesseract
+import openai
 import re
 from src import pbot
+# Set up OpenAI API credentials
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-# Define function to get character name from image using OCR
+# Define function to get character name from image using OpenAI
 def get_character_name(image_url):
-    # Download image and read text using OCR
+    # Download image and create OpenAI API request
     image_data = requests.get(image_url).content
-    text = pytesseract.image_to_string(io.BytesIO(image_data), lang='eng')
+    prompt = f"Identify the character in this image: {image_url}"
+    response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.5)
     
-    # Extract possible character names from text using regular expressions
-    character_names = re.findall(r'[A-Z][a-z]+\s?[A-Z][a-z]+', text)
+    # Extract possible character names from OpenAI response using regular expressions
+    character_names = re.findall(r'[A-Z][a-z]+\s?[A-Z][a-z]+', response.choices[0].text)
     
     # Return the first character name found, if any
     if character_names:
@@ -20,22 +24,22 @@ def get_character_name(image_url):
     else:
         return None
 
-# Define callback function to handle messages
-@pbot.on_message()
-def handle_message(client, message):
-    # Check if message is a command and a reply to a photo or sticker
-    if message.text and message.text.startswith('/identify') and message.reply_to_message and (message.reply_to_message.photo or message.reply_to_message.sticker):
+# Define command handler function
+@pbot.on(telethon.events.NewMessage(pattern="/identify"))
+async def handle_command(event):
+    # Check if message is a reply to a photo or sticker
+    if event.is_reply and (await event.get_reply_message()).photo or (await event.get_reply_message()).sticker:
         # Check if the replied message is a sticker and download it if necessary
-        if message.reply_to_message.sticker:
-            sticker = message.reply_to_message.sticker
-            image_url = f"https://api.telegram.org/file/bot{client.token}/{sticker.file_id}.webp"
+        if (await event.get_reply_message()).sticker:
+            sticker = await (await event.get_reply_message()).download_media()
+            image_url = f"data:image/webp;base64,{sticker}"
         else:
-            photo = message.reply_to_message.photo[-1]
-            image_url = f"https://api.telegram.org/file/bot{client.token}/{photo.file_id}.jpg"
-        # Get the character name using OCR
+            photo = await (await event.get_reply_message()).download_media()
+            image_url = f"data:image/jpeg;base64,{photo}"
+        # Get the character name using OpenAI
         character_name = get_character_name(image_url)
         # Reply with the character name
         if character_name:
-            message.reply(f"The character in this photo/sticker is {character_name}")
+            await event.reply(f"The character in this photo/sticker is {character_name}")
         else:
-            message.reply("Could not identify any characters in this photo/sticker")
+            await event.reply("Could not identify any characters in this photo/sticker")
