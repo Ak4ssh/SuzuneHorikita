6cd8b94afe1f2247f9e7cad0ec7074859aecfa39
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-03-02 16:01:47 +0530

    Update sax.py

--- src/source/sax.py
+++ src/source/sax.py
@@ -1,30 +1,32 @@
-import pyrogram
-import os
+from telethon import TelegramClient, events
 from src import pbot
 
-# Set up counters for groups and users
+# Initialize counters for groups and users
 group_count = 0
 user_count = 0
 
-# Define a handler for when the bot is added to a group
-@pbot.on_chat_members_added()
-async def on_chat_member_join(client, chat):
-    global group_count
-    if chat.type == 'group' or chat.type == 'supergroup':
-        # Increment the group counter
-        group_count += 1
-    
-# Define a handler for when a message is received
-@pbot.on_message()
-async def on_message(client, message):
+# Define event handlers for when the bot is added to a group or a user sends a message
+@pbot.on(events.NewMessage(incoming=True))
+async def handle_new_message(event):
+    # Increment the user count whenever a message is received
     global user_count
-    # Check if the message was sent by a user and not a bot
-    if message.from_user and not message.from_user.is_bot:
-        # Increment the user counter
-        user_count += 1
-        
-# Define a command handler to show the group and user counts
-@pbot.on_message(pyrogram.filters.command('stat', prefixes='/'))
-async def stats(client, message):
+    user_count += 1
+
+@pbot.on(events.ChatAction())
+async def handle_chat_action(event):
+    # Increment the group count whenever the bot is added to a group
+    if event.user_added and event.chat_id > 2867:
+        global group_count
+        group_count += 1
+
+# Define a command handler for the !stats command
+@pbot.on(events.NewMessage(pattern='^/stat'))
+async def handle_stats_command(event):
+    # Get the current number of groups and users
     global group_count, user_count
-    await message.reply_text(f'Total groups: {group_count + 2867}\nTotal users: {user_count + 2579237}')
+    num_groups = group_count - 2867
+    num_users = user_count - 2579237
+
+    # Send a message with the current stats
+    message = f"Bot is currently added to {num_groups} groups and serving {num_users} users"
+    await event.respond(message)
