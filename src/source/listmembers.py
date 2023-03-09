90eb30445d231fff1d5b2f5a67c44717c1146723
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-03-09 11:37:00 +0530

    Update listmembers.py

--- src/source/listmembers.py
+++ src/source/listmembers.py
@@ -1,23 +1,24 @@
 from pyrogram import Client, filters
 import csv
 from src import pbot as app
+import asyncio
 
 # Define the command to trigger the member listing
 @app.on_message(filters.command(["listmembers"]))
-def list_members(client, message):
+async def list_members(client, message):
     
     # Get the chat ID of the current chat
     chat_id = message.chat.id
     
     # If the command was sent in a group, get the chat members
     if message.chat.type == "group" or message.chat.type == "supergroup":
-        members = client.get_chat_members(chat_id)
+        members = await client.get_chat_members(chat_id)
     
     # If the command included a group username, get the chat members of that group
     elif len(message.command) > 1:
         username = message.command[1]
-        chat = client.get_chat(username)
-        members = client.get_chat_members(chat.id)
+        chat = await client.get_chat(username)
+        members = await client.get_chat_members(chat.id)
         
     # If the command was not sent in a group and no group username was provided, return an error message
     else:
@@ -32,5 +33,4 @@ def list_members(client, message):
             writer.writerow([member.user.id, member.user.first_name, member.user.last_name, member.user.username])
 
     # Reply to the user with the file
-    message.reply_document(document="members.txt", caption="Here is the list of members.")
-    
+    await message.reply_document(document="members.txt", caption="Here is the list of members.")
