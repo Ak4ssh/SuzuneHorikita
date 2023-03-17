e28a9c03bcf1aaf1ce88b7a297b5572e3d9802d9
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-03-17 22:02:17 +0530

    Update mediaAlert.py

--- src/source/mediaAlert.py
+++ src/source/mediaAlert.py
@@ -5,7 +5,7 @@ from src import telethn as client
 chat_id = -1001680514362
 
 # Define the event handler function for file/media messages
-@client.on(events.NewMessage(chats=chat_id, incoming=True, media=True))
+@client.on(events.NewMessage(chats=chat_id, incoming=True, document=lambda d: d.mime_type.startswith('image/') or d.mime_type.startswith('video/')))
 async def handle_new_media(event):
     # Get the media file name and message link
     file_name = event.media.document.attributes[0].file_name
