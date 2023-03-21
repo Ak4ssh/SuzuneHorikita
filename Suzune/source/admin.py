5bc2b7283217322b07a86f226098a39945c66bbb
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-03-21 13:26:50 +0530

    Update admin.py

--- Suzune/source/admin.py
+++ Suzune/source/admin.py
@@ -1,38 +1,44 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserAdminInvalid
+from Suzune import suzune as app
 
-admin_filter = filters.create(lambda _, __, message: message.from_user.is_admin)
-
-@Client.on_message(filters.command("ban") & admin_filter)
+# define the command to ban a user
+@app.on_message(filters.command("ban") & filters.group)
 def ban_user(client, message):
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-    elif len(message.command) > 1:
-        user_id = message.command[1]
+    # check if user is an admin
+    if message.from_user.is_admin:
+        # get the user to ban
+        if message.reply_to_message:
+            user_id = message.reply_to_message.from_user.id
+        elif len(message.command) == 2:
+            user_id = message.command[1]
+        else:
+            message.reply("Usage: /ban (user id) /ban @username /ban (replied user)")
+            return
+
+        # ask for confirmation with an inline button
+        confirm_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Yes", callback_data=f"ban_{user_id}"), InlineKeyboardButton("No", callback_data="cancel")]
+        ])
+        message.reply(f"Are you sure you want to ban {user_id}?", reply_markup=confirm_markup)
     else:
-        message.reply("Please specify a user to ban.")
-        return
+        message.reply("You must be an admin to use this command.")
 
-    keyboard = [
-        [InlineKeyboardButton("Yes", callback_data=f"ban_{user_id}"), InlineKeyboardButton("No", callback_data="cancel_ban")]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    message.reply(f"Are you sure you want to ban user {user_id}?", reply_markup=reply_markup)
+# handle inline button callbacks
+@app.on_callback_query()
+def handle_callback(client, callback_query):
+    if callback_query.data.startswith("ban_"):
+        # get the user to ban from the callback data
+        user_id = int(callback_query.data.split("_")[1])
 
-@Client.on_callback_query(filters.regex(r"^ban_(\d+)$"))
-def confirm_ban(client, callback_query):
-    chat_id = callback_query.message.chat.id
-    user_id = callback_query.from_user.id
-    chat_member = client.get_chat_member(chat_id, user_id)
-    if not chat_member.is_admin:
-        callback_query.answer("You are not an admin.")
-        return
-
-    banned_user_id = int(callback_query.matches[0].group(1))
-    client.kick_chat_member(chat_id, banned_user_id)
-    callback_query.answer("User banned.")
-
-@Client.on_callback_query(filters.regex(r"^cancel_ban$"))
-def cancel_ban(client, callback_query):
-    callback_query.answer("Ban cancelled.")
-
+        # try to ban the user
+        try:
+            client.kick_chat_member(callback_query.message.chat.id, user_id)
+            callback_query.answer("User has been banned.")
+        except UserAdminInvalid:
+            callback_query.answer("I can't ban that user because they're an admin.")
+    elif callback_query.data == "cancel":
+        callback_query.answer("Action canceled.")
+    else:
+        callback_query.answer("Unknown callback data.")
