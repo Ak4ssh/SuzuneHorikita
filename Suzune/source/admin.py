41780da0117d4b3156720a1fa3c9aad1edfd79f1
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-03-21 13:55:29 +0530

    Update admin.py

--- Suzune/source/admin.py
+++ Suzune/source/admin.py
@@ -3,6 +3,18 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserAdminInvalid
 from Suzune import suzune as app
 
+# define the admincache function to cache group admins
+admins_cache = {}
+def admincache(client, chat_id):
+    if chat_id not in admins_cache:
+        admins = []
+        members = client.iter_chat_members(chat_id)
+        for member in members:
+            if member.status in ("administrator", "creator"):
+                admins.append(member.user.id)
+        admins_cache[chat_id] = admins
+    return admins_cache[chat_id]
+
 # define the command to ban a user
 @app.on_message(filters.command("ban") & filters.group)
 def ban_user(client, message):
@@ -10,24 +22,25 @@ def ban_user(client, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
 
-    chat_member = client.get_chat_member(chat_id, user_id)
-    if chat_member.status in ["creator", "administrator"]:
-        # get the user to ban
-        if message.reply_to_message:
-            user_id = message.reply_to_message.from_user.id
-        elif len(message.command) == 2:
-            user_id = message.command[1]
-        else:
-            message.reply("Usage: /ban (user id) /ban @username /ban (replied user)")
-            return
-
-        # ask for confirmation with an inline button
-        confirm_markup = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Yes", callback_data=f"ban_{user_id}"), InlineKeyboardButton("No", callback_data="cancel")]
-        ])
-        message.reply(f"Are you sure you want to ban {user_id}?", reply_markup=confirm_markup)
-    else:
+    if user_id not in admincache(client, chat_id):
         message.reply("You must be an admin to use this command.")
+        return
+
+    # get the user to ban
+    if message.reply_to_message:
+        user_id = message.reply_to_message.from_user.id
+    elif len(message.command) == 2:
+        user_id = message.command[1]
+    else:
+        message.reply("Usage: /ban (user id) /ban @username /ban (replied user)")
+        return
+
+    # ask for confirmation with an inline button
+    confirm_markup = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Yes", callback_data=f"ban_{user_id}"), InlineKeyboardButton("No", callback_data="cancel")]
+    ])
+    message.reply(f"Are you sure you want to ban {user_id}?", reply_markup=confirm_markup)
+
 
 # handle inline button callbacks
 @app.on_callback_query()
