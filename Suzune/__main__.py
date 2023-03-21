0c5f0dae527c475a33b9372ec8ba81cb358fc7cd
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2023-03-21 13:09:31 +0530

    Update __main__.py

--- Suzune/__main__.py
+++ Suzune/__main__.py
@@ -1,15 +1,12 @@
-import asyncio
-
+import sys
 from pyrogram import idle
-
 from . import suzune
-               
-async def startup():
-    # STARTING CLIENTS
-    await suzune.start()
 
-print("started!")
-    
-loop = asyncio.get_event_loop()
 if __name__ == "__main__":
-    loop.run_until_complete(startup())
+    try:
+       suzune.start()
+       print("Bot Started!")
+    except Exception as eror:
+       print(str(error))
+       sys.exit()
+    idle()
