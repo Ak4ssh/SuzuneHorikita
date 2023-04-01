642b9776725ab019096c335ad34de118699e5222
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-04-01 21:49:33 +0530

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -1,15 +1,12 @@
-import asyncio
-
+import sys
 from pyrogram import idle
-from src import suzune
+from . import suzune
 
-print("Starting Suzune !")
-    # STARTING BOT CLIENT
-await suzune.start()
-
-
-
-
-loop = asyncio.get_event_loop()
 if __name__ == "__main__":
-    loop.run_until_complete(startup())
+    try:
+       suzune.start()
+       print("Bot Started!")
+    except Exception as eror:
+       print(str(error))
+       sys.exit()
+    idle()
