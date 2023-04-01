5ecf488d6e1549d2ac45827da3404a114e74b13c
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-04-01 21:57:32 +0530

    Update admin.py

--- Suzune/source/admin.py
+++ Suzune/source/admin.py
@@ -1,44 +1,48 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.errors import (ChatAdminRequired, PeerIdInvalid, RightForbidden,
+                             RPCError, UserAdminInvalid)
+from pyrogram.enums import ChatMembersFilter as cmf
 from Suzune import suzune as app
 
-# define a filter to check if the user is an admin in the chat
-def is_admin(chat_id, user_id):
-    chat_member = app.get_chat_member(chat_id, user_id)
-    return chat_member.status in ("creator", "administrator")
-
-# define a command handler for /ban
-@app.on_message(filters.command("ban", prefixes="/") & filters.private)
+# define the ban command handler
+@app.on_message(filters.command("ban") & filters.group & filters.user("self"))
 def ban_user(client, message):
-    # check if the user is an admin in the chat
-    if not is_admin(message.chat.id, message.from_user.id):
-        client.send_message(message.chat.id, "You are not an admin in this chat.")
+    # check if the user is an admin in the group
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    try:
+        chat_members = client.get_chat_members(chat_id, filter=cmf.ADMINISTRATORS)
+        admins = [member.user.id for member in chat_members]
+        if user_id not in admins:
+            message.reply_text("Sorry, you are not an admin in this group.")
+            return
+    except ChatAdminRequired:
+        message.reply_text("Sorry, I need to be an admin in this group to execute this command.")
+        return
+    except RPCError as e:
+        message.reply_text(f"Sorry, an error occurred: {e}")
         return
 
-    # get the user ID or username from the command or replied message
-    if len(message.command) > 2:
-        user_id = message.command[1]
-        ban_reason = " ".join(message.command[2:])
-    elif message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        ban_reason = " ".join(message.command[1:])
-    else:
-        # no user ID or username specified
+    # check if a user was provided to ban
+    if not message.reply_to_message:
+        message.reply_text("Please provide a user to ban.")
         return
 
-    # check if the user ID is a number (i.e., it's a user ID)
-    if user_id.isdigit():
-        user_id = int(user_id)
-    else:
-        # it's not a user ID, so it must be a username
-        user = client.get_users(user_id)
-        user_id = user.id
+    # get the user to ban
+    user = message.reply_to_message.from_user
 
     # ban the user
-    client.kick_chat_member(message.chat.id, user_id)
-
-    # send a message to the chat to announce that the user has been banned
-    client.send_message(
-        message.chat.id, 
-        f"The user {user_id} has been banned from the chat for the following reason: {ban_reason}"
-    )
+    try:
+        client.kick_chat_member(chat_id, user.id)
+        message.reply_text(f"{user.username} has been banned from the group.")
+    except UserAdminInvalid:
+        message.reply_text("Sorry, I cannot ban an admin in this group.")
+    except RightForbidden:
+        message.reply_text("Sorry, I do not have the necessary rights to ban users in this group.")
+    except PeerIdInvalid:
+        message.reply_text("Sorry, the user ID is invalid.")
+    except RPCError as e:
+        message.reply_text(f"Sorry, an error occurred: {e}")
+    else:
+        message.reply_text(f"{user.username} has been banned from the group.")
