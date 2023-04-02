823316fcf90c623faab7ceb7fa4b50f103f26e2a
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2023-04-02 12:12:33 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 if path.exists("config.env"):
     load_dotenv("config.env")
 
-TOKEN = environ.get("TOKEN", None)
+BOT_TOKEN = environ.get("BOT_TOKEN", None)
 API_ID = int(environ.get("API_ID", 6))
 API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 Dev = [int(x) for x in environ.get("Dev", "").split()]
