787b45d9f382bedcfe1f6ca1c9e6bf9f38f86e2c
desinobita <akashtix445@gmail.com>
2023-04-02 11:31:10 +0530

    Deep Commit (Pyro)

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM archlinux/archlinux:latest
+
+# Install dependencies
+RUN pacman -Syu --noconfirm && pacman -S --noconfirm git wget libxml2 libxslt zip python-pip ffmpeg
+
+# Downloading mongodb tools
+RUN wget https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2004-x86_64-100.5.2.tgz && tar -xf mongodb*.tgz && \ 
+  mv mongodb-database-tools-ubuntu2004-x86_64-100.5.2/bin/* /bin/ && \
+  rm -rf mongodb-database-tools-ubuntu2004-x86_64-100.5.2*
+
+# Changing working directory and it's permission
+WORKDIR /app
+RUN chmod 777 /app
+
+# Install python dependencies
+COPY requirements.txt .
+RUN pip3 install --no-cache-dir -U -r requirements.txt
+
+# Copy files to the working directory
+COPY . .
+
+# Run the application
+CMD ["python3","-m","Suzune"]
---
+++ sample_config.py
@@ -0,0 +1,22 @@
+from os import environ, path
+
+from dotenv import load_dotenv
+
+if path.exists("config.env"):
+    load_dotenv("config.env")
+
+Token = environ.get("Token", None)
+API_ID = int(environ.get("API_ID", 6))
+API_HASH = environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+Dev = [int(x) for x in environ.get("Dev", "").split()]
+LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
+GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
+MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
+WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
+MONGO_URL = environ.get("MONGO_URL", None)
+ARQ_API_URL = environ.get("ARQ_API_URL", None)
+ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
+RSS_DELAY = int(environ.get("RSS_DELAY", None))
+UPSTREAM_REPO = environ.get(
+    "UPSTREAM_REPO", "https://github.com/DesiNobita/SuzuneBot.git"
+)
