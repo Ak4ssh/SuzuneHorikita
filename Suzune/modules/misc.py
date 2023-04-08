bf12c8f960ebb1c3f06d9b945d460513ac886674
Akash Tiwari <84653553+Ak4ssh@users.noreply.github.com>
2023-04-08 18:24:52 +0530

    Update misc.py

--- Suzune/modules/misc.py
+++ Suzune/modules/misc.py
@@ -129,7 +129,7 @@ async def rtfm(_, message):
 logs_channel_id = -1001442340523
 
 # Handle new chat members
-@app.on_chat_member_joined()
+@app.on_chat_member_updated()
 async def on_chat_member_updated(client, message, new_members):
     # Check if the bot itself is one of the new members
     for member in new_members:
