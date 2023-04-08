2945dac6cd15af4a8dd0b28cc4097ac151bc6210
Akash Tiwari <84653553+Ak4ssh@users.noreply.github.com>
2023-04-08 19:36:18 +0530

    Update start.py

--- Suzune/modules/start.py
+++ Suzune/modules/start.py
@@ -8,7 +8,7 @@ async def start_command(client, message):
     video_file_id = "https://graph.org/file/1f2a07be0b1bc76f75fe3.mp4"
     caption = f"Heya {message.from_user.first_name}, My name is Suzune Horikita - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.\n\nJoin my [News Channel](http://t.me/SuzuneSuperbot) to get information on all the latest updates."
     keyboard = InlineKeyboardMarkup(
-        [[InlineKeyboardButton("Add me to your group", url="https://telegram.me/YOUR_BOT_USERNAME?startgroup=new")]]
+        [[InlineKeyboardButton("Add me to your group", url="https://telegram.me/Suzune_Superbot?startgroup=new")]]
     )
     await app.send_video(chat_id=message.chat.id, video=video_file_id, caption=caption, reply_markup=keyboard)
 
