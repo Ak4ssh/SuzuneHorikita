0bdaae2e1295b7963872c3f4d6cdd0b53479853c
Akash Tiwari <84653553+Ak4ssh@users.noreply.github.com>
2023-04-09 10:45:50 +0530

    Update start.py

--- src/source/start.py
+++ src/source/start.py
@@ -1,26 +1,18 @@
-import os
-import re
-from platform import python_version as kontol
-from telethon import events, Button
-from telegram import __version__ as telever
-from telethon import __version__ as tlhver
-from pyrogram import __version__ as pyrover
-from src.events import register
-from src import telethn as tbot
-from src import pbot
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMediaVideo
+from src import pbot as app
 
-VENOM = "https://telegra.ph/file/7c97605753018dfa4f832.mp4"
 
-@register(pattern=("/start"))
-async def awake(event):
-  TEXT = f"Heya [{event.sender.first_name}](tg://user?id={event.sender.id}), My name is Suzune Horikita - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.\n\n"
-  TEXT += "Join my [News Channel](t.me/SuzuneSuperbot) to get information on all the latest updates."
-  BUTTON = [[Button.url("➕Add Me To Your Group➕", "https://t.me/Suzune_Superbot?startgroup=true"),]]
-  await tbot.send_file(event.chat_id, VENOM, caption=TEXT, buttons=BUTTON)   
+@app.on_message(filters.command(["start"]))
+async def start_command(client, message):
+    video_file_id = "https://graph.org/file/1f2a07be0b1bc76f75fe3.mp4"
+    caption = f"Heya {message.from_user.first_name}, My name is Suzune Horikita - I'm here to help you manage your groups! Hit /help to find out more about how to use me to my full potential.\n\nJoin my [News Channel](http://t.me/SuzuneSuperbot) to get information on all the latest updates."
+    keyboard = InlineKeyboardMarkup(
+        [[InlineKeyboardButton("Add me to your group", url="https://telegram.me/Suzune_Superbot?startgroup=new")]]
+    )
+    await app.send_video(chat_id=message.chat.id, video=video_file_id, caption=caption, reply_markup=keyboard)
 
-@pbot.on_message(filters.command("play"))
+@app.on_message(filters.command("play"))
 def handle_music(client, message):
     # create an inline keyboard with a button
     button = InlineKeyboardButton(
